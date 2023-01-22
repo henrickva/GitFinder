5a4357f1fa8e75f42e73d90a83c651f884ac2a4b
Henrick Amaral <henrick.vaz.amaral@gmail.com>
2023-01-22 15:20:39 -0300

    adicionando -usuario nao encontrado

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,13 @@ import Search from "./Search"
 import { useState } from "react"
 import { UserProps } from "../types/user"
 import User from "./User"
+import Error from "./Error"
 
 export default function App() {
 
   const [user, setUser] = useState<UserProps|null>(null)
+
+  const[error, setError] = useState(false)
 
   const loadUser = async(userName:string) =>{
     const res = await fetch(`https://api.github.com/users/${userName}`)
@@ -16,6 +19,13 @@ export default function App() {
 
     const userData: UserProps = {avatar_url, login, location, followers, following}
 
+    setError(false)
+    setUser(null)
+    
+    if (res.status === 404){
+      setError(true)
+      return
+  }
     setUser(userData)
   }
 
@@ -23,6 +33,7 @@ export default function App() {
     <div>
       <Search loadUser={loadUser}/>
       {user && <User{...user}/>}
+      {error && <Error />}
     </div>
   )
 }
---
+++ src/components/Error.tsx
@@ -0,0 +1,6 @@
+
+export default function Error(){
+    return (
+        <p>Usuário Não Encontrado !</p>
+    )
+}
